a49d117f53d82b2d9bcd177cc246e2ab0133e394
Nitin <nitin.khandelwal@tothenew.com>
2021-02-24 12:51:18 +0530

    completed Q16

--- Q16.java
+++ Q16.java
@@ -1,5 +1,58 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Q16 {
+    private final Lock lock1 = new ReentrantLock(true);
+    private final Lock lock2 = new ReentrantLock(true);
+
+    public void task1(){
+        lock1.lock();
+        System.out.println("lock1 acquired, waiting to acquire lock2.");
+        try {
+            Thread.sleep(50);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+
+        lock2.lock();
+        System.out.println("lock2 acquired");
+
+        System.out.println("executing first operation.");
+
+        lock2.unlock();
+        lock1.unlock();
+    }
+
+    public void task2() {
+        while (true) {
+            try {
+                lock2.tryLock(50, TimeUnit.MICROSECONDS);
+                System.out.println("lock2 acquired, trying to acquire lock1.");
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (lock1.tryLock()) {
+                System.out.println("lock1 acquired.");
+            } else {
+                System.out.println("cannot acquire lock1, releasing lock2.");
+                lock2.unlock();
+                continue;
+            }
+
+            System.out.println("executing second operation.");
+            break;
+        }
+        lock1.unlock();
+        lock2.unlock();
+    }
+
     public static void main(String[] args) {
-        
+        Q16 deadlock = new Q16();
+        new Thread(deadlock::task1, "T1").start();
+        new Thread(deadlock::task2, "T2").start();
     }
 }
